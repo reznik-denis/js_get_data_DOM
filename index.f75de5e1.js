9b495212b220ef3a764cabc1862ff7a3c5f6b965
Denis Reznik <reznik.d.o12@gmail.com>
2024-06-19 05:41:55 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,14 @@
+"use strict";
+let total = 0;
+const allPopulation = [
+    ...document.querySelectorAll(".population")
+].map((item)=>{
+    const el = parseInt(item.innerHTML.split(",").join(""));
+    total += el;
+    return el;
+});
+const average = total / allPopulation.length;
+document.querySelector(".total-population").textContent = total.toLocaleString("en-US");
+document.querySelector(".average-population").textContent = average.toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
